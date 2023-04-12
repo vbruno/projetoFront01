df3f1e60487aa7fd00325dde6e0034293354c100
Bruno S Velho <bruno.velho@gmail.com>
2023-04-12 19:30:55 -0300

    Init

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import style from "./App.module.css";
+
+export function App() {
+  return (
+    <article className={style.container}>
+      <div className={style.card}>
+        <img
+          src="https://images.unsplash.com/photo-1605379399642-870262d3d051?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=350&q=50"
+          alt="Cover"
+        />
+        <img
+          className={style.avatar}
+          src="https://github.com/vbruno.png"
+          alt="Avatar"
+        />
+        <h1>Descrição/nome</h1>
+        <h1>Stack</h1>
+      </div>
+    </article>
+  );
+}
