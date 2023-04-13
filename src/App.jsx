71a24729b298b0f5710544ea89c06e35c144a5bf
Bruno S Velho <bruno.velho@gmail.com>
2023-04-13 16:38:10 -0300

    Ajustes

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import style from "./App.module.css";
 
 export function App() {
   return (
-    <article className={style.container}>
+    <div className={style.container}>
       <div className={style.card}>
         <img
           src="https://images.unsplash.com/photo-1605379399642-870262d3d051?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=350&q=50"
@@ -13,9 +13,15 @@ export function App() {
           src="https://github.com/vbruno.png"
           alt="Avatar"
         />
-        <h1>Descrição/nome</h1>
-        <h1>Stack</h1>
+        <p className={style.p1}>Bruno S Velho</p>
+        <p className={style.p2}>Web Developer</p>
+        <div className={style.stack}>
+          <span>#JavaScript</span>
+          <span>#Native</span>
+          <span>#Native</span>
+          <span>#Native</span>
+        </div>
       </div>
-    </article>
+    </div>
   );
 }
