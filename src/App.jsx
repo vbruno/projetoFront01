945086ea9a335f6f7ebb66fae88388f2387f6669
Bruno S Velho <bruno.velho@gmail.com>
2023-04-14 22:28:16 -0300

    Componentes

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,25 @@
 import style from "./App.module.css";
 
+import { CardPerson } from "./components/CardPerson";
+
 export function App() {
+  const listStack = [
+    { Tec: "JavaScript", color: "#3d3d3d" },
+    { Tec: "HTML" },
+    { Tec: "CSS" },
+    { Tec: "Typescript" },
+    { Tec: "React" },
+  ];
+
   return (
     <div className={style.container}>
-      <div className={style.card}>
-        <img
-          src="https://images.unsplash.com/photo-1605379399642-870262d3d051?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=350&q=50"
-          alt="Cover"
-        />
-        <img
-          className={style.avatar}
-          src="https://github.com/vbruno.png"
-          alt="Avatar"
-        />
-        <p className={style.p1}>Bruno S Velho</p>
-        <p className={style.p2}>Web Developer</p>
-        <div className={style.stack}>
-          <span>#JavaScript</span>
-          <span>#Native</span>
-          <span>#Native</span>
-          <span>#Native</span>
-        </div>
-      </div>
+      <CardPerson
+        cover="https://images.unsplash.com/photo-1605379399642-870262d3d051?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=350&q=50"
+        avatar="https://github.com/vbruno.png"
+        name="Bruno S Velho"
+        office="Web Developer"
+        stacks={listStack}
+      />
     </div>
   );
 }
